c0e2297ad543da622c64b853ebfeb4eb2efc7f08
kuddy <admin@kuddy.cn>
2020-06-15 18:17:03 +0800

    feat(telegraf): 添加芯片run状态

--- telegraf/scripts/minknow/flow_cell_stats.py
+++ telegraf/scripts/minknow/flow_cell_stats.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-import os
 import sys
 
 sys.path.append('/opt/grpc')
@@ -10,9 +9,11 @@ import minknow.rpc.manager_pb2 as manager_pb2
 import minknow.rpc.manager_pb2_grpc as manager_pb2_grpc
 import minknow.rpc.device_pb2 as device_pb2
 import minknow.rpc.device_pb2_grpc as device_pb2_grpc
+import minknow.rpc.protocol_pb2 as protocol_pb2
+import minknow.rpc.protocol_pb2_grpc as protocol_pb2_grpc
 
 import grpc
-
+from grpc._channel import _InactiveRpcError
 from common import BaseCommand
 
 
@@ -25,21 +26,29 @@ class FlowCellStatsCommand(BaseCommand):
     def handle(self, args):
         hosts = set(args.minknow_hosts.split(','))
         for host in hosts:
+
             devices = self.get_devices(host)
             for device in devices.active:
+                running = self.get_protocol_running_status(host, device)
                 channel = grpc.insecure_channel('%s:%s' % (host, device.ports.insecure_grpc))
                 stub = device_pb2_grpc.DeviceServiceStub(channel)
                 device_request = device_pb2.GetFlowCellInfoRequest()
                 flow_cell_info = stub.get_flow_cell_info(device_request)
+                if flow_cell_info.has_flow_cell:
+                    if running:
+                        status = 2
+                    else:
+                        status = 3
+                else:
+                    status = 1
                 print('flow_cell_info,machine=P1,device=%s status=%s,product_code="%s",flow_cell_id="%s",x=%s,y=%s' % (
                     device.name,
-                    2 if flow_cell_info.has_flow_cell else 1,
+                    status,
                     flow_cell_info.product_code,
                     flow_cell_info.flow_cell_id,
                     device.layout.x,
                     device.layout.y
                 ))
-
 
     def get_devices(self, host):
         manager_stub = self.manager_stub(host)
@@ -51,6 +60,21 @@ class FlowCellStatsCommand(BaseCommand):
         manager_channel = grpc.insecure_channel('%s:9501' % host)
         manager_stub = manager_pb2_grpc.ManagerServiceStub(manager_channel)
         return manager_stub
+
+    def get_protocol_running_status(self, host, device):
+        channel = grpc.insecure_channel('%s:%s' % (host, device.ports.insecure_grpc))
+        stub = protocol_pb2_grpc.ProtocolServiceStub(channel)
+        request = protocol_pb2.GetRunInfoRequest()
+        try:
+            info = stub.get_current_protocol_run(request)
+            if info.run_id:
+                return True
+            else:
+                return False
+        except _InactiveRpcError as e:
+            if not e.details() == 'No protocol running':
+                raise e
+            return False
 
 
 if __name__ == '__main__':
